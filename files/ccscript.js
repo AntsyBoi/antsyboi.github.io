06a390987ae049ea799e71ecee41bfb1e1051a00
Ant <38162305+AntsyBoi@users.noreply.github.com>
2021-05-22 18:16:42 -0500

    added killswitch to ccscript

--- files/ccscript.js
+++ files/ccscript.js
@@ -1,1 +1,18 @@
-killswitch = 0; function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }; s2 = async function(amount, time){while(true = 1){ if(killswitch = 1){break;}; await sleep(time); Game.cookies = Game.cookies+amount}};
+s1 = 1;
+killswitch = 0;
+function sleep(ms)
+{
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+s2 = async function(amount, time)
+{
+  while(s1 = 1)
+  {
+    if(killswitch == 1)
+    {
+      killswitch = 0
+      break;
+    };
+    await sleep(time); Game.cookies = Game.cookies+amount
+  }
+};
