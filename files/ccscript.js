cc98ebc63139befee32d5cbf07878cce7e6302cb
Ant <38162305+AntsyBoi@users.noreply.github.com>
2021-05-22 17:45:47 -0500

    Update ccscript.js

--- files/ccscript.js
+++ files/ccscript.js
@@ -1,5 +1,1 @@
-s1 = 1;
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-s2 = async function(amount){while(s1=1){await sleep(200); Game.cookies = Game.cookies+amount}};
+s1 = 1; function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }; s2 = async function(amount){while(s1=1){await sleep(200); Game.cookies = Game.cookies+amount}};
