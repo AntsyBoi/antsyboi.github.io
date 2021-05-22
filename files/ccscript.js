1c162f2304b5733de0c0fd13d059f330992928fb
Ant <38162305+AntsyBoi@users.noreply.github.com>
2021-05-22 17:35:29 -0500

    added cookie clicker script

---
+++ files/ccscript.js
@@ -0,0 +1,5 @@
+s1 = 1;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+s2 = async function(amount){while(s1=1){await sleep(200); Game.cookies = Game.cookies+amount}};
